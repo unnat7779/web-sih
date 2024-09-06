aa72b700db97f6adb785e36ed647b52243d4c99b
Unnat Agrawal <agrawalunnat7779@gmail.com>
2024-09-06 09:28:15 +0530

    offsite

--- web/components/offlocation.js
+++ web/components/offlocation.js
@@ -14,43 +14,27 @@ const Offlocation = ({ employee }) => {
       <div className="flex flex-col ml-3">
         <div className="absolute top-2">
           <div className="flex">
-            <h1 className="text-2xl font-bold m-4 text-nowrap">{employee.name}</h1>
-            <h1 className="text-xl font-semibold m-4 font-mono text-[#605b5b]">
-              {employee.employeeId}
-            </h1>
+            <h1 className="text-2xl font-bold m-4 text-nowrap">Location</h1>
+            
           </div>
           <div className="flex flex-col -mt-2">
             <div className="flex gap-[70px]">
               <span className="ml-4">Check-In</span>
-              <span>{employee.checkIn}</span>
+              
             </div>
-            <div className="flex gap-[44px]">
-              <span className="ml-4">Check-Out</span>
-              <span>{employee.checkOut}</span>
-            </div>
-            <div className="flex gap-[70px]">
-              <span className="ml-4">Working Hours</span>
-              <span>{employee.workingHours}</span>
-            </div>
+            
           </div>
         </div>
-        <div className="bg-[#fbefef] p-3 rounded-xl -mb-4 mt-[120px]">
+        <div className="bg-[#332e2e] p-3 rounded-xl -mb-4 mt-[120px]">
           <div className="flex">
-            <h1 className="text-lg font-bold ml-4 text-nowrap">Contact</h1>
+            <h1 className="text-lg font-bold ml-4 text-nowrap">ADDRESS</h1>
           </div>
           <div className="flex flex-col">
             <div className="flex gap-[70px]">
               <span className="ml-4">Email</span>
-              <span>: {employee.email}</span>
+              
             </div>
-            <div className="flex gap-[44px]">
-              <span className="ml-4">Mob. No.</span>
-              <span>: {employee.mobile}</span>
-            </div>
-            <div className="flex gap-[62px]">
-              <span className="ml-4">Address</span>
-              <span>: {employee.address}</span>
-            </div>
+            
           </div>
         </div>
       </div>
@@ -58,23 +42,14 @@ const Offlocation = ({ employee }) => {
         <div className="bg-[#c4917b] rounded-xl">
           <div className="flex">
             <h1 className="text-xl font-bold m-4 text-nowrap">Total Attendance -</h1>
-            <span className="text-2xl font-bold m-4 text-nowrap font-mono">{employee.totalAttendance}</span>
+            <span className="text-2xl font-bold m-4 text-nowrap font-mono"></span>
           </div>
           <div className="flex -mt-5">
             <h1 className="text-xl font-bold m-4 text-nowrap">Number of Leaves -</h1>
-            <span className="text-2xl font-bold mt-4 mb-4 mr-4 ml-2 text-nowrap font-mono">{employee.leaves}</span>
+            <span className="text-2xl font-bold mt-4 mb-4 mr-4 ml-2 text-nowrap font-mono"></span>
           </div>
         </div>
-        <div className="flex mt-4 justify-between">
-          <div className="bg-[#d18679] flex flex-col justify-center items-center rounded-xl px-3 h-20">
-            <h1 className="text-xl font-medium m-4 text-nowrap">Onsite </h1>
-            <h1 className="text-4xl font-bold m-4 text-nowrap -mt-3 font-mono">{employee.onsite}</h1>
-          </div>
-          <div className="bg-[#d18679] flex flex-col justify-center items-center rounded-xl px-3 h-20">
-            <h1 className="text-xl font-medium m-4 text-nowrap">Offsite </h1>
-            <h1 className="text-4xl font-bold m-4 text-nowrap -mt-3 font-mono">{employee.offsite}</h1>
-          </div>
-        </div>
+        
         <div className="mt-2 m-auto mt-5">
           <button className="bg-[#e06565] p-2 rounded-xl">
             <div className="flex flex-wrap gap-x-2 px-2">
